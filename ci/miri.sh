858ec3ac36cd3c7d4f0f1099632d07d12943b9f9
al8n <scygliu1@gmail.com>
2022-08-07 19:44:59 +0800

    fix miri

--- ci/miri.sh
+++ ci/miri.sh
@@ -5,7 +5,7 @@ rustup toolchain install nightly --component miri
 rustup override set nightly
 cargo miri setup
 
-export MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-disable-isolation -Zmiri-allow-communication"
+export MIRIFLAGS="-Zmiri-strict-provenance -Zmiri-disable-isolation"
 
 cargo miri test --no-default-features --target x86_64-unknown-linux-gnu
 cargo miri test --no-default-features --target aarch64-unknown-linux-gnu
